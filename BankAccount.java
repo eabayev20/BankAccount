5d1402f89e800d773767065e6847ec860116c9ce
eabayev20 <eabayev20@gmail.com>
2020-10-27 18:15:51 -0400

    I realized my files weren't saved last commit

---
+++ BankAccount.java
@@ -0,0 +1,16 @@
+public class BankAccount {
+
+  private double balance;
+  private int accountID;
+  private String password;
+  public bank( int a, String p ) {
+    balance = 0.0;
+    accountID = a;
+    password = p;
+
+  }
+  
+
+
+
+}
